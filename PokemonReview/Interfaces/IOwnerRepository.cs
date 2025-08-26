a8a0b3bf4cc08d21613867789e8b935e1ae183c0
haidermuhammed <haidermuhammadk99@gmail.com>
2025-08-26 22:05:44 +0300

    feat: Implemented interface owner repo

--- PokemonReview/Interfaces/IOwnerRepository.cs
+++ PokemonReview/Interfaces/IOwnerRepository.cs
@@ -1,6 +1,17 @@
+using PokemonReview.Models;
+
 namespace PokemonReview.Interfaces;
 
-public class IOwnerRepository
+public interface IOwnerRepository
 {
+    ICollection<Owner> GetOwners();
     
+    Owner GetOwner(int ownerId);
+    
+    ICollection<Owner> GetOwnerOfAPokemon(int pokeId);
+    
+    ICollection<Pokemon> GetPokemonByOwner (int ownerId);
+    
+    bool OwnerExists(int ownerId);
+
 }
